9bc70541061685741897861a8e005816ed88521b
WilhelmBuitrago <122063634+WilhelmBuitrago@users.noreply.github.com>
2025-06-05 14:37:42 -0500

    v1.2

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from chatbot_rag.RAG import RAG
-from chatbot_rag.chat import Chatbot
+from chatbot_rag.chat import *
 from chatbot_rag.preprocessing import PyMuPDFPreprocessing, BasePreprocessing
 import os
 import shutil
@@ -13,6 +13,28 @@ class ChatApp:
     def __init__(self):
         st.set_page_config(page_title="Interactive Chatbot", layout="centered")
         st.title("Interactive Chatbot")
+        with st.expander("Model configuration"):
+            host = st.selectbox(label="Host", options=["Ollama", "Hugginface"], index=0)
+            if host == "Ollama":
+                model_name = st.selectbox(
+                    "Model:", options=[model.model for model in ollama.list()["models"]]
+                )
+
+                if not hasattr(self, "name") or model_name != self.name:
+                    self.name = model_name
+                    self.load_bot(host, model_name=self.name)
+            elif host == "Hugginface":
+                model_name = st.text_input(
+                    label="Model Name", value="deepseek-ai/DeepSeek-V3-0324"
+                )
+                token = st.text_input(label="Token", value="")
+                provider = st.text_input(label="Provider", value="hyperbolic")
+                st.button(
+                    "Load Model",
+                    on_click=self.load_bot,
+                    args=(host, model_name, token, provider),
+                )
+
         if "bot" not in st.session_state:
             st.session_state.bot = None
         if "rag" not in st.session_state:
@@ -23,13 +45,13 @@ class ChatApp:
         if os.path.exists("./info/"):
             shutil.rmtree("./info/")
 
-        self.opciones_sidebar()
+        self.sidebar_options()
 
         self.init_session()
 
-        self.reintento_hecho = False  # para manejar reintentos de timeout
+        self.retry_done = False  # to handle timeout retries
 
-    async def llamar_bot_con_timeout(self, bot, context, question, timeout=120):
+    async def call_bot_with_timeout(self, bot, context, question, timeout=120):
         try:
             loop = asyncio.get_event_loop()
             return await asyncio.wait_for(
@@ -40,9 +62,9 @@ class ChatApp:
                 timeout,
             )
         except asyncio.TimeoutError:
-            return None  # indica que se agotó el tiempo
+            return None  # indicates timeout
 
-    def cargar_rag(self, preprocessing=None, **kwargs):
+    def load_rag(self, preprocessing=None, **kwargs):
         if preprocessing == "PyMuPDFPreprocessing":
             preprocessing = PyMuPDFPreprocessing
             st.session_state.rag = RAG(
@@ -52,25 +74,24 @@ class ChatApp:
             st.session_state.rag = RAG(path="./data/")
         st.session_state.rag()
 
-    def cargar_bot(self, model_name):
-        st.session_state.bot = Chatbot(name=model_name)
-
-    def opciones_sidebar(self):
-        with st.sidebar:
-            st.header("Opciones")
-            model_name = st.selectbox(
-                "Model:", options=[model.model for model in ollama.list()["models"]]
+    def load_bot(self, host, model_name, token=None, provider=None):
+        if host == "Ollama":
+            st.session_state.bot = OllamaChatbot(name=model_name)
+        elif host == "Hugginface":
+            st.session_state.bot = HuggingFaceChatbot(
+                model_name=model_name, token=token, provider=provider
             )
 
-            if not hasattr(self, "name") or model_name != self.name:
-                self.name = model_name
-                self.cargar_bot(self.name)
-            self.accion_rag = st.selectbox(
+    def sidebar_options(self):
+        with st.sidebar:
+            st.header("Options")
+
+            self.rag_action = st.selectbox(
                 "RAG Actions:",
                 options=["BasePreprocessing", "PyMuPDFPreprocessing"],
                 index=0,
             )
-            if self.accion_rag == "PyMuPDFPreprocessing":
+            if self.rag_action == "PyMuPDFPreprocessing":
                 with st.expander("PyMuPDF Configuration"):
                     st.write(
                         "You can configure image extraction and other parameters. (Only text by default)"
@@ -81,16 +102,16 @@ class ChatApp:
                         self.tesseract_path = st.text_input(label="Tesseract Path")
                     self.extract_tables = st.checkbox("Extract tables", value=False)
 
-            self.archivo_subido = st.file_uploader(
+            self.uploaded_file = st.file_uploader(
                 "Upload a document (PDF)", type=["pdf"]
             )
             if st.button("Process document"):
-                if self.archivo_subido:
+                if self.uploaded_file:
                     os.makedirs("./data", exist_ok=True)
-                    if not os.path.exists(f"./data/{self.archivo_subido.name}"):
-                        with open(f"./data/{self.archivo_subido.name}", "wb") as f:
-                            f.write(self.archivo_subido.getbuffer())
-                    if self.accion_rag == "PyMuPDFPreprocessing":
+                    if not os.path.exists(f"./data/{self.uploaded_file.name}"):
+                        with open(f"./data/{self.uploaded_file.name}", "wb") as f:
+                            f.write(self.uploaded_file.getbuffer())
+                    if self.rag_action == "PyMuPDFPreprocessing":
                         kwargs = {
                             "extract_images": self.extract_images,
                             "extract_tables": self.extract_tables,
@@ -98,9 +119,9 @@ class ChatApp:
                         if self.extract_images:
                             kwargs["tesseract_path"] = self.tesseract_path
 
-                        self.cargar_rag(preprocessing=self.accion_rag, **kwargs)
+                        self.load_rag(preprocessing=self.rag_action, **kwargs)
                     else:
-                        self.cargar_rag(preprocessing=self.accion_rag)
+                        self.load_rag(preprocessing=self.rag_action)
                 else:
                     if os.path.exists("./data/"):
                         shutil.rmtree("./data/")
@@ -113,7 +134,7 @@ class ChatApp:
         if "messages" not in st.session_state:
             st.session_state.messages = []
 
-    def mostrar_css(self):
+    def display_css(self):
         st.markdown(
             """
             <style>
@@ -152,83 +173,83 @@ class ChatApp:
             unsafe_allow_html=True,
         )
 
-    def mostrar_historial(self):
+    def display_history(self):
         for msg in st.session_state.messages:
             if msg["role"] == "user":
-                self.mostrar_mensaje_usuario(msg["content"])
+                self.display_user_message(msg["content"])
             else:
-                self.mostrar_mensaje_bot(msg["content"])
+                self.display_bot_message(msg["content"])
 
-    def mostrar_mensaje_usuario(self, contenido):
+    def display_user_message(self, content):
         st.markdown(
             f"""
             <div class="chat-row chat-right">
-                <div class="chat-bubble user-bubble">{contenido}</div>
+                <div class="chat-bubble user-bubble">{content}</div>
                 <div class="avatar"></div>
             </div>
             """,
             unsafe_allow_html=True,
         )
 
-    def mostrar_mensaje_bot(self, contenido):
+    def display_bot_message(self, content):
         st.markdown(
             f"""
             <div class="chat-row chat-left">
                 <div class="avatar"></div>
-                <div class="chat-bubble bot-bubble">{contenido}</div>
+                <div class="chat-bubble bot-bubble">{content}</div>
             </div>
             """,
             unsafe_allow_html=True,
         )
 
-    def manejar_entrada(self):
+    def handle_input(self):
         user_input = st.chat_input("Write your message...")
         if user_input:
             st.session_state.messages.append({"role": "user", "content": user_input})
-            self.mostrar_mensaje_usuario(user_input)
+            self.display_user_message(user_input)
             if st.session_state.rag is None:
-                context = """None"""
+                context = None
             else:
                 context = st.session_state.rag._search_context(user_input)
 
             respuesta = asyncio.run(
-                self.llamar_bot_con_timeout(st.session_state.bot, context, user_input)
+                self.call_bot_with_timeout(st.session_state.bot, context, user_input)
             )
             if respuesta is None:
-                # primer timeout, reintenta
-                if not self.reintento_hecho:
-                    self.reintento_hecho = True
+                # first timeout, retry
+                if not self.retry_done:
+                    self.retry_done = True
                     respuesta = asyncio.run(
-                        self.llamar_bot_con_timeout(
+                        self.call_bot_with_timeout(
                             st.session_state.bot, context, user_input
                         )
                     )
                     if respuesta is None:
-                        respuesta = self.llamar_bot_con_timeout(
+                        respuesta = self.call_bot_with_timeout(
                             st.session_state.bot,
                             """Say "I'm sorry, I haven't been able to generate an answer yet. Is there anything else I can assist you with?" In the same language as the user's question""",
                             user_input,
                         )
                 else:
-                    respuesta = self.llamar_bot_con_timeout(
+                    respuesta = self.call_bot_with_timeout(
                         st.session_state.bot,
                         """Say "I'm sorry, I haven't been able to generate an answer yet. Is there anything else I can assist you with?" In the same language as the user's question""",
                         user_input,
                     )
             else:
-                self.reintento_hecho = False  # reinicia si fue exitoso
+                self.retry_done = False  # reset if successful
             st.session_state.messages.append(
                 {"role": "assistant", "content": respuesta}
             )
-            self.mostrar_mensaje_bot(respuesta)
+            self.display_bot_message(respuesta)
 
-    def ejecutar(self):
-        self.mostrar_css()
-        self.mostrar_historial()
-        self.manejar_entrada()
+    def run(self):
+        self.display_css()
+        self.display_history()
+        self.handle_input()
 
 
-# Ejecutar app
+# Run app
 if __name__ == "__main__":
     app = ChatApp()
-    app.ejecutar()
+    app.run()
